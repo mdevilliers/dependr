e26ed2241b3062749b7d9a9e0f95e9fe8e02064f
Mark deVilliers <mark.devilliers@adarga.ai>
2022-11-23 15:40:05 +0000

    reverse lookup table

--- pkg/dependabot/ecosystems.go
+++ pkg/dependabot/ecosystems.go
@@ -11,7 +11,7 @@ import (
 
 type (
 	ecosystems struct {
-		files map[string][]string
+		files map[string]string
 	}
 )
 
@@ -20,22 +20,35 @@ var (
 	// https://docs.github.com/en/code-security/dependabot/dependabot-version-updates/configuration-options-for-the-dependabot.yml-file#package-ecosystem
 	// https://docs.github.com/en/code-security/supply-chain-security/understanding-your-software-supply-chain/about-the-dependency-graph#supported-package-ecosystems
 	wellKnown = ecosystems{
-		files: map[string][]string{
-			"bundler":      {"Gemfile.lock", "Gemfile"}, // TODO: support *.gemspec
-			"cargo":        {"Cargo.toml", "Cargo.lock"},
-			"composer":     {"composer.json", "composer.lock"},
-			"docker":       {"Dockerfile"}, // TODO: support 'artisinal' dockerfile names
-			"hex":          {"mix.exs"},
-			"elm":          {"elm-package.json"},
-			"gitsubmodule": {".gitmodules"},
-			// "github-actions" : {}, // NOTE: handled as an exception
-			"gomod":     {"go.mod", "go.sum"},
-			"gradle":    {"build.gradle"},
-			"maven":     {"pom.xml"},
-			"npm":       {"package-lock.json", "package.json", "yarn.lock"},
-			"nuget":     {".csproj", ".vbproj", ".nuspec", ".vcxproj", ".fsproj", "packages.config"},
-			"pip":       {"requirements.txt", "pipfile", "pipfile.lock", "setup.py"},
-			"terraform": {".terraform.lock.hcl"},
+		files: map[string]string{
+			"Gemfile.lock":        "bundler",
+			"Gemfile":             "bundler", // TODO: support *.gemspec
+			"Cargo.toml":          "cargo",
+			"Cargo.lock":          "cargo",
+			"composer.json":       "composer",
+			"composer.lock":       "composer",
+			"Dockerfile":          "docker", // TODO: support 'artisinal' dockerfile names
+			"mix.exs":             "hex",
+			"elm-package.json":    "elm",
+			".gitmodules":         "gitsubmodule",
+			"go.mod":              "gomod",
+			"go.sum":              "gomod",
+			"build.gradle":        "gradel",
+			"pom.xml":             "maven",
+			"package-lock.json":   "npm",
+			"package.json":        "npm",
+			"yarn.lock":           "npm",
+			".csproj":             "nuget",
+			".vbproj":             "nuget",
+			".nuspec":             "nuget",
+			".vcxproj":            "nuget",
+			".fsproj":             "nuget",
+			"packages.config":     "nuget",
+			"requirements.txt":    "pip",
+			"pipfile":             "pip",
+			"pipfile.lock":        "pip",
+			"setup.py":            "pip",
+			".terraform.lock.hcl": "terraform",
 		},
 	}
 )
@@ -68,18 +81,15 @@ func (n *node) Scan() error {
 				return fmt.Sprintf("./%s", i)
 			}
 
-			for k, v := range wellKnown.files {
-				for _, wk := range v {
-					if wk == info.Name() {
-						rel, err := filepath.Rel(n.repo.root, path)
-						if err != nil {
-							return err
-						}
-						rel = filepath.Dir(rel)
-						update := newUpdate(k, dot(rel))
-						updates[key(update)] = update
-					}
+			v, found := wellKnown.files[info.Name()]
+			if found {
+				rel, err := filepath.Rel(n.repo.root, path)
+				if err != nil {
+					return err
 				}
+				rel = filepath.Dir(rel)
+				update := newUpdate(v, dot(rel))
+				updates[key(update)] = update
 			}
 
 			return nil
